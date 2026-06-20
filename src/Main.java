a261e25a0c6528876d3a23d1bd44788db88a19e5
jacob <boltonjb@mail.uc.edu>
2026-06-20 12:04:42 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        // create a Scanner to read from the console
+        // declare an int for the birth month
+        // prompt for birth month (1 - 12)
+        // if the input is a valid int:
+        //     read the month and clear the buffer
+        //     if month is 1 through 12 -> echo "Your birth month is: N"
+        //     else -> "You entered an incorrect month value: N"
+        // else: read the bad input as a String and show an error
+
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+        String trash = "";
+
+        System.out.print("Enter your birth month (1 - 12): ");
+
+        if (in.hasNextInt()) {
+            birthMonth = in.nextInt();
+            in.nextLine();   // clear the buffer
+
+            if (birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("Your birth month is: " + birthMonth);
+            } else {
+                System.out.println("You entered an incorrect month value: " + birthMonth);
+            }
+        } else {
+            trash = in.nextLine();
+            System.out.println("That is not a whole number: " + trash);
+            System.out.println("Run the program again and enter a value from 1 to 12!");
+        }
+    }
 }
